35f3406df542fb9ad9f06645dee0795f83faa5f9
mikkelJurs <s224279@student.dtu.dk>
2023-02-28 12:41:15 +0100

    Initial commit with custom GUI.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,9 @@
 module com.example.smtpmailclient_datacommf23 {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+    requires java.desktop;
+
+
     opens com.example.smtpmailclient_datacommf23 to javafx.fxml;
     exports com.example.smtpmailclient_datacommf23;
 }
