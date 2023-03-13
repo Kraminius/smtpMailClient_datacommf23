d149d6569af22f7a38dde0e95dd4912bafcee140
TwoFacedToby <tobiasgoerlyk@hotmail.com>
2023-03-13 11:31:59 +0100

    imprted files comes along with envelope

--- src/main/java/com/example/smtpmailclient_datacommf23/MailClient.java
+++ src/main/java/com/example/smtpmailclient_datacommf23/MailClient.java
@@ -1,5 +1,7 @@
 package com.example.smtpmailclient_datacommf23;
 
+import javafx.stage.Stage;
+
 import java.io.*;
 import java.net.*;
 import java.awt.*;
@@ -14,70 +16,75 @@ import java.awt.event.*;
  */
 public class MailClient extends Frame {
     /* The stuff for the GUI. */
-    private Button btSend = new Button("Send");
-    private Button btClear = new Button("Clear");
-    private Button btQuit = new Button("Quit");
-    private Label serverLabel = new Label("Local mailserver:");
-    private TextField serverField = new TextField("smtp.gmail.com", 40);
-    private Label fromLabel = new Label("From:");
-    private TextField fromField = new TextField("datacommg09@gmail.com", 40);
-    private Label toLabel = new Label("To:"); 
-    private TextField toField = new TextField("s221064@dtu.dk", 40);
-    private Label subjectLabel = new Label("Subject:");
-    private TextField subjectField = new TextField("hej", 40);
-    private Label messageLabel = new Label("Message:");
-    private TextArea messageText = new TextArea(10, 40);
+	private Button btSend = new Button("Send");
+	private Button btClear = new Button("Clear");
+	private Button btQuit = new Button("Quit");
+	private Label serverLabel = new Label("Local mailserver:");
+	private TextField serverField = new TextField("datacomm.bhsi.xyz", 40);
+	private Label fromLabel = new Label("From:");
+	private TextField fromField = new TextField("s221064@dtu.dk", 40);
+	private Label toLabel = new Label("To:");
+	private TextField toField = new TextField("s221064@dtu.dk", 40);
+	private Label subjectLabel = new Label("Subject:");
+	private TextField subjectField = new TextField("hej", 40);
+	private Label messageLabel = new Label("Message:");
+	private TextArea messageText = new TextArea(10, 40);
 	private Button importButton = new Button("Import Files");
 	private Label importedLabel = new Label("none");
 
-    /**
-     * Create a new MailClient window with fields for entering all
-     * the relevant information (From, To, Subject, and message).
-     */
-    public MailClient() {
-	super("Java Mailclient");
-	
+	/**
+	 * Create a new MailClient window with fields for entering all
+	 * the relevant information (From, To, Subject, and message).
+	 */
+	private Stage stage;
+	public MailClient() {
+		super("Java Mailclient");
+
 	/* Create panels for holding the fields. To make it look nice,
 	   create an extra panel for holding all the child panels. */
-	Panel serverPanel = new Panel(new BorderLayout());
-	Panel fromPanel = new Panel(new BorderLayout());
-	Panel toPanel = new Panel(new BorderLayout());
-	Panel subjectPanel = new Panel(new BorderLayout());
-	Panel messagePanel = new Panel(new BorderLayout());
-	serverPanel.add(serverLabel, BorderLayout.WEST);
-	serverPanel.add(serverField, BorderLayout.CENTER);
-	fromPanel.add(fromLabel, BorderLayout.WEST);
-	fromPanel.add(fromField, BorderLayout.CENTER);
-	toPanel.add(toLabel, BorderLayout.WEST);
-	toPanel.add(toField, BorderLayout.CENTER);
-	subjectPanel.add(subjectLabel, BorderLayout.WEST);
-	subjectPanel.add(subjectField, BorderLayout.CENTER);
-	messagePanel.add(messageLabel, BorderLayout.NORTH);	
-	messagePanel.add(messageText, BorderLayout.CENTER);
-	Panel fieldPanel = new Panel(new GridLayout(0, 1));
-	fieldPanel.add(serverPanel);
-	fieldPanel.add(fromPanel);
-	fieldPanel.add(toPanel);
-	fieldPanel.add(subjectPanel);
+		Panel serverPanel = new Panel(new BorderLayout());
+		Panel fromPanel = new Panel(new BorderLayout());
+		Panel toPanel = new Panel(new BorderLayout());
+		Panel subjectPanel = new Panel(new BorderLayout());
+		Panel messagePanel = new Panel(new BorderLayout());
+		Panel importPanel = new Panel(new BorderLayout());
+		serverPanel.add(serverLabel, BorderLayout.WEST);
+		serverPanel.add(serverField, BorderLayout.CENTER);
+		fromPanel.add(fromLabel, BorderLayout.WEST);
+		fromPanel.add(fromField, BorderLayout.CENTER);
+		toPanel.add(toLabel, BorderLayout.WEST);
+		toPanel.add(toField, BorderLayout.CENTER);
+		subjectPanel.add(subjectLabel, BorderLayout.WEST);
+		subjectPanel.add(subjectField, BorderLayout.CENTER);
+		messagePanel.add(messageLabel, BorderLayout.NORTH);
+		messagePanel.add(messageText, BorderLayout.CENTER);
+		importPanel.add(importButton, BorderLayout.WEST);
+		importPanel.add(importedLabel, BorderLayout.CENTER);
+		Panel fieldPanel = new Panel(new GridLayout(0, 1));
+		fieldPanel.add(serverPanel);
+		fieldPanel.add(fromPanel);
+		fieldPanel.add(toPanel);
+		fieldPanel.add(subjectPanel);
+		fieldPanel.add(importPanel);
 
 
 	/* Create a panel for the buttons and add listeners to the
 	   buttons. */
-	Panel buttonPanel = new Panel(new GridLayout(1, 0));
-	btSend.addActionListener(new SendListener());
-	btClear.addActionListener(new ClearListener());
-	btQuit.addActionListener(new QuitListener());
-	buttonPanel.add(btSend);
-	buttonPanel.add(btClear);
-	buttonPanel.add(btQuit);
-	
-	/* Add, pack, and show. */
-	add(fieldPanel, BorderLayout.NORTH);
-	add(messagePanel, BorderLayout.CENTER);
-	add(buttonPanel, BorderLayout.SOUTH);
-	pack();
-	show();
-    }
+		Panel buttonPanel = new Panel(new GridLayout(1, 0));
+		btSend.addActionListener(new SendListener());
+		btClear.addActionListener(new ClearListener());
+		btQuit.addActionListener(new QuitListener());
+		importButton.addActionListener(new ImportListener());
+		buttonPanel.add(btSend);
+		buttonPanel.add(btClear);
+		buttonPanel.add(btQuit);
+		/* Add, pack, and show. */
+		add(fieldPanel, BorderLayout.NORTH);
+		add(messagePanel, BorderLayout.CENTER);
+		add(buttonPanel, BorderLayout.SOUTH);
+		pack();
+		show();
+	}
 
     static public void main(String argv[]) {
 	new MailClient();
